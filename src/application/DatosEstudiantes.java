0a980bdeda53c3d4cbf1d6f5d9581084fb8d5131
Jorge14G <111819876+Jorge14G@users.noreply.github.com>
2022-08-22 21:23:26 -0600

    Segunda versión: Se crea la tabla sobre la cual se desplegarán los datos, cuyos títulos de columna están basados en la clase DatosEstudiantes, ls abstracción detalla a través de una serie de atributos las notas y un conjunto de datos personales según estudiante

---
+++ src/application/DatosEstudiantes.java
@@ -0,0 +1,130 @@
+package application;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class DatosEstudiantes{
+	private final SimpleStringProperty Carne;
+	private final SimpleStringProperty Telefono;
+	private final SimpleStringProperty NomApe; 
+	private final SimpleStringProperty Correo; 
+	private final SimpleStringProperty Nickname; 
+	protected final SimpleStringProperty NotPromExamenes; 
+	protected final SimpleStringProperty NotPromQuices; 
+	protected final SimpleStringProperty NotPromTareas; 
+	protected final SimpleStringProperty NotProyec1; 
+	protected final SimpleStringProperty NotProyec2; 
+	protected final SimpleStringProperty NotProyec3;
+	protected final SimpleStringProperty Tipo;
+	private final SimpleStringProperty NotPromExamQuizTarea;
+	private final SimpleStringProperty NotPromProyects;
+	private final SimpleStringProperty NotFinal;
+
+	public DatosEstudiantes(String Carne, String Telefono, String NomApe, String Correo, String Nickname, String Tipo,String NotPromExamenes, String NotPromQuices, String NotPromTareas, String NotProyec1, String NotProyec2, String NotProyec3, String NotPromExamQuizTarea, String NotPromProyects, String NotFinal){
+		this.Carne = new SimpleStringProperty(Carne);
+		this.Telefono = new SimpleStringProperty(Telefono);
+		this.NomApe = new SimpleStringProperty(NomApe);
+		this.Correo = new SimpleStringProperty(Correo);
+		this.Nickname = new SimpleStringProperty(Nickname);
+		this.NotPromExamenes = new SimpleStringProperty(NotPromExamenes);
+		this.NotPromQuices = new SimpleStringProperty(NotPromQuices);
+		this.NotPromTareas = new SimpleStringProperty(NotPromTareas);
+		this.NotProyec1 = new SimpleStringProperty(NotProyec1);
+		this.NotProyec2 = new SimpleStringProperty(NotProyec2);
+		this.NotProyec3 = new SimpleStringProperty(NotProyec3);
+		this.Tipo = new SimpleStringProperty(Tipo);
+		this.NotPromExamQuizTarea = new SimpleStringProperty(NotPromExamQuizTarea);
+		this.NotPromProyects = new SimpleStringProperty(NotPromProyects);
+		this.NotFinal = new SimpleStringProperty(NotFinal);
+
+	}
+	public String getCarne(){
+		return Carne.get();
+	}
+	public void setCarne(String x) {
+		Carne.set(x);
+	}
+	public String getTelefono(){
+		return Telefono.get();
+	}
+	public void setTelefono(String x) {
+		Telefono.set(x);
+	}
+	public String getNomApe(){
+		return NomApe.get();
+	}
+	public void setNomApe(String x) {
+		NomApe.set(x);
+	}
+	public String getCorreo(){
+		return Correo.get();
+	}
+	public void setCorreo(String x) {
+		Correo.set(x);
+	}
+	public String getNickname(){
+		return Nickname.get();
+	}
+	public void setNickname(String x) {
+		Nickname.set(x);
+	}
+	public String getNotPromExamenes(){
+		return NotPromExamenes.get();
+	}
+	public void setNotPromExamenes(String x) {
+		NotPromExamenes.set(x);
+	}
+	public String getNotPromQuices(){
+		return NotPromQuices.get();
+	}
+	public void setNotPromQuices(String x) {
+		NotPromQuices.set(x);
+	}
+	public String getNotPromTareas(){
+		return NotPromTareas.get();
+	}
+	public void setNotPromTareas(String x) {
+		NotPromTareas.set(x);
+	}
+	public String getNotProyec1(){
+		return NotProyec1.get();
+	}
+	public void setNotProyec1(String x) {
+		NotProyec1.set(x);
+	}
+	public String getNotProyec2(){
+		return NotProyec2.get();
+	}
+	public void setNotProyec2(String x) {
+		NotProyec2.set(x);
+	}
+	public String getNotProyec3(){
+		return NotProyec3.get();
+	}
+	public void setNotProyec3(String x) {
+		NotProyec3.set(x);
+	}
+	public String getTipo(){
+		return Tipo.get();
+	}
+	public void setTipo(String x) {
+		Tipo.set(x);
+	}
+	public String getNotPromExamQuizTarea(){
+		return NotPromExamQuizTarea.get();
+	}
+	public void setNotPromExamQuizTarea(String x) {
+		NotPromExamQuizTarea.set(x);
+	}
+	public String getNotPromProyects(){
+		return NotPromProyects.get();
+	}
+	public void setNotPromProyects(String x) {
+		NotPromProyects.set(x);
+	}
+	public String getNotFinal(){
+		return NotFinal.get();
+	}
+	public void setNotFinal(String x) {
+		NotFinal.set(x);
+	}
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -5,11 +5,19 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.geometry.Pos;
 
 public class Main extends Application {
+	private TableView <DatosEstudiantes> tablaNotas;
+	private TableColumn<DatosEstudiantes, String> colNotPromExamQuizTarea;
+	private TableColumn<DatosEstudiantes, String> colNotPromProyects;
+	private TableColumn<DatosEstudiantes, String> colNotFinal;
+	
 	public void start(Stage Window) {
 		VBox root = new VBox();
 		HBox root2 = new HBox();
@@ -21,7 +29,58 @@ public class Main extends Application {
 		Label nombreArchivo = new Label("                  ");
 		Button buscarArchivo = new Button("Buscar Archivo");
 		
-		root.getChildren().addAll(title, root2);
+		
+		tablaNotas = new TableView<>();
+
+		TableColumn<DatosEstudiantes, String> colCarne = new TableColumn<>("Carné");
+		colCarne.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("Carne"));
+
+		TableColumn<DatosEstudiantes, String> colTelefono = new TableColumn<>("Teléfono");
+		colTelefono.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("Telefono"));
+
+		TableColumn<DatosEstudiantes, String> colNomApe = new TableColumn<>("Nombre Completo");
+		colNomApe.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("NomApe"));
+
+		TableColumn<DatosEstudiantes, String> colCorreo = new TableColumn<>("Correo");
+		colCorreo.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("Correo"));
+
+		TableColumn<DatosEstudiantes, String> colNickname = new TableColumn<>("Nickname");
+		colNickname.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("Nickname"));
+
+		TableColumn<DatosEstudiantes, String> colTipoEstudiante = new TableColumn<>("Tipo");
+		colTipoEstudiante.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("Tipo"));
+
+		TableColumn<DatosEstudiantes, String> colNotPromExamenes = new TableColumn<>("Prom.Exámenes");
+		colNotPromExamenes.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("NotPromExamenes"));
+
+		TableColumn<DatosEstudiantes, String> colNotPromQuices = new TableColumn<>("Prom.Quices");
+		colNotPromQuices.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("NotPromQuices"));
+
+		TableColumn<DatosEstudiantes, String> colNotPromTareas = new TableColumn<>("Prom.Tareas");
+		colNotPromTareas.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("NotPromTareas"));
+
+		TableColumn<DatosEstudiantes, String> colNotProyec1 = new TableColumn<>("Proyecto #1");
+		colNotProyec1.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("NotProyec1"));
+
+		TableColumn<DatosEstudiantes, String> colNotProyec2 = new TableColumn<>("Proyecto #2");
+		colNotProyec2.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("NotProyec2"));
+
+		TableColumn<DatosEstudiantes, String> colNotProyec3 = new TableColumn<>("Proyecto #3");
+		colNotProyec3.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("NotProyec3"));
+
+		colNotPromExamQuizTarea = new TableColumn<>("Nota Promedio (Exámenes, Quices, Tareas)");
+		colNotPromExamQuizTarea.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("NotPromExamQuizTarea"));
+
+		colNotPromProyects = new TableColumn<>("Nota Promedio (Proyectos)");
+		colNotPromProyects.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("NotPromProyects"));
+
+		colNotFinal = new TableColumn<>("Nota Final");
+		colNotFinal.setCellValueFactory(new PropertyValueFactory <DatosEstudiantes, String>("NotFinal"));
+
+		tablaNotas.getColumns().addAll(colCarne, colTelefono, colNomApe, colCorreo, colNickname, colTipoEstudiante, colNotPromExamenes, colNotPromQuices, colNotPromTareas, colNotProyec1, colNotProyec2, colNotProyec3);
+
+		
+		root.getChildren().addAll(title, root2, tablaNotas);
 		root2.getChildren().addAll(nombreArchivo, buscarArchivo);
 		root.setAlignment(Pos.TOP_CENTER);
 		root2.setAlignment(Pos.TOP_CENTER);
